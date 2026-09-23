bfa19d407a3c5b7ce5e0fbcd06a6fc75ef7b8dc2
MDRASHED <mohammedrashedmohiuddin55@gmail.com>
2026-09-23 16:33:37 +0530

    new files

---
+++ script.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div", {}, [
+  React.createElement("h1", { id: "h1-react" }, "Hello world from react"),
+  React.createElement("h2", {id: "h2-react"}, "Hello h2"),
+  React.createElement("div", {id: "child-div", className: "child"}, [
+    React.createElement("h1", {id:"child-div1", className: "child"}, "I am h1 of child-div"),
+    React.createElement('h2', {id:"child-div2", className: "child"}, "I am h2 of child-div")
+  ])
+]);
+
+
+
+let root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(parent);
